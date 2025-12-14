5860ac4cb65c1ec0a1325c99dcfc956bc06c0483
saponalisaju <saponalibpl12486@gmail.com>
2025-12-14 10:02:06 +0600

    lkl

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,22 @@
 module.exports = {
   root: true,
-  extends: ['next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react-perf'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'next/core-web-vitals'],
   rules: {
     'no-console': 'off', // disable console warnings
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'react-perf/jsx-no-new-function-as-prop': 'off',
+    'react-perf/jsx-no-new-object-as-prop': 'off',
+    '@next/next/no-img-element': 'off', // allow <img> without warning
   },
 };
